363576feb71fecabab1e2bad31f6a1ebceab541e
Todd Papke <sabulatodd@mac.com>
2017-10-16 11:50:56 -0500

    Initial Commit

---
+++ GithubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by Todd Papke on 10/16/17.
+//  Copyright © 2017 Todd Papke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
